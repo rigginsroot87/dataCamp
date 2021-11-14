a0d527ef4b3afb48fddabc25b5e37b7b92904c56
rigginsroot87 <31332320+rigginsroot87@users.noreply.github.com>
2021-11-14 08:48:00 -0700

    Updated notes from datacampe
    Intermediate python notes.

---
+++ Data Camp Notes.py
@@ -0,0 +1,9 @@
+#MATPLOTLIB
+import matplotlib as plt
+list1 = [1,2,3,4,5]
+list2 = [4.0,6.0,8.0,6.0,7.5]
+#you can do a line plot with the code below, don't forget the show call
+plt.plot(list1,list2)
+plt.show()
+# clears the plot so you can 
+plt.clf()
