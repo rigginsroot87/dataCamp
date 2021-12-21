7f777a4f10a600190f33700e75b0f9ae36e2e788
rigginsroot87 <jeremiah.root@gmail.com>
2021-12-21 10:05:59 -0700

    added indices notes

--- Indices in Pandas.py
+++ Indices in Pandas.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 df_dict = {'a':[1,2,3,4,5,6,7,8,9,10],'b': ["red","blue","yellow","purlple",'yellow','orange','green','black','white','teal'],\
-           'c':[0,0,0,4,4,7,7,7,7,8], 'date' :pd.date_range('2015-02-24', periods=10, freq='D')}
+           'c':[0,0,0,4,4,7,7,7,7,8], 'date' :pd.date_range('2015-02-24', periods=10, freq='M')}
 df_dummy = pd.DataFrame.from_dict(df_dict)
 
 
 print(df_dummy)
 df_dummy_date_ind = df_dummy.set_index('date')
 print(df_dummy_date_ind)
+#slicing over date indices seems really useful
+print(df_dummy_date_ind.loc['2015-03':'2015-06'])
